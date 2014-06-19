823fde62f6a874aa2db0ae39c16b5f237d552b5a
ingmrb <ingmrb@users.noreply.github.com>
2014-06-19 16:09:20 -0300

    compilatex 4.0

--- compilatex.sh
+++ compilatex.sh
@@ -4,21 +4,19 @@
 # Para usarlo, ejecutar en la Terminal "sh compilatex.sh" (el script y el archivo de LaTeX tienen que estar en la misma carpeta).
 
 # Para terminar de ejecutar el script, tipear en la Terminal "Ctrl + Shift + c".
-# Si el archivo no compila, arreglar el error y escribir "quit" (o "q") en la Terminal. 
-# El compilador va a tratar de compilarlo de nuevo. Si compila, el pdf se va a seguir actualizando cada vez que se guarde!
 
 
 read -p "Ingrese el nombre del archivo .TeX a compilar: " archivo
 
 
-pdflatex  "$archivo.tex"
+pdflatex -interaction=nonstopmode -halt-on-error "$archivo.tex"
 fecha=$(date -r "$archivo.tex" +%T)
 
 
 while true 
     do
        if [ "$fecha" != "$(date -r "$archivo.tex" +%T)" ]; then    
-       		pdflatex  "$archivo.tex"
+       		pdflatex -interaction=nonstopmode -halt-on-error "$archivo.tex"
        		fecha=$(date -r "$archivo.tex" +%T)
        fi 
        sleep 1
