ea0a3d1c7542c76cbf67b751b957a95ddbba0585
ingmrb <ingmrb@users.noreply.github.com>
2014-06-19 17:55:58 -0300

    compilatex 5.0

--- compilatex.sh
+++ compilatex.sh
@@ -6,18 +6,37 @@
 # Para terminar de ejecutar el script, tipear en la Terminal "Ctrl + Shift + c".
 
 
+
+# BetterPDFLaTeX colors pdflatex warnings in orange, errors in red and the "Output written ..." message in green. 
+# Duplicate lines (which appear right after each other) are skipped as well as those annoying only-bracket-lines, which pdflatex generates a lot. 
+# The --line-buffered parameter makes sure that the output is still "smooth" and not "laggy" due to buffering of grep. 
+# The --color=always (in contrast to --color=auto) preserves the colors of previous grep calls when piping them together like this.
+
+# You can put the script in your PATH with filename pdflatex, such that all Makefiles which call pdflatex use this colored version. 
+
+# Fuente: fw42.blogspot.com.ar/2012/08/colored-pdflatex-console-output.html
+
+BetterPDFLaTeX(){
+GREP="grep --color=always --line-buffered"
+(/usr/bin/pdflatex -file-line-error $* 2>&1) \
+ | $GREP -vP "^\s*((\[|\]|\(|\))\s*)+$" \
+ | GREP_COLOR="01;31" $GREP -P "(^[^:]*:\d+: )|(^l\.\d+ )|(^! LaTeX Error: )|$" \
+ | GREP_COLOR="00;33" $GREP -P "(LaTeX Warning(:|))|(Package [^\s]+ Warning(:|))|$" \
+ | GREP_COLOR="00;32" $GREP -P "^Output written on .*\.pdf \(.*\)\.$|$" \
+ | uniq
+} 
+
+
 read -p "Ingrese el nombre del archivo .TeX a compilar: " archivo
 
-
-pdflatex -interaction=nonstopmode -halt-on-error "$archivo.tex"
+BetterPDFLaTeX "$archivo.tex" -interaction=nonstopmode -halt-on-error
 fecha=$(date -r "$archivo.tex" +%T)
 
 
 while true 
     do
        if [ "$fecha" != "$(date -r "$archivo.tex" +%T)" ]; then    
-       		pdflatex -interaction=nonstopmode -halt-on-error "$archivo.tex"
+       		BetterPDFLaTeX "$archivo.tex" -interaction=nonstopmode -halt-on-error
        		fecha=$(date -r "$archivo.tex" +%T)
        fi 
-       sleep 1
     done
