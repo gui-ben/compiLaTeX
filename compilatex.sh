273cb95111e26689556a28bd61ef08d4806f954d
ingmrb <ingmrb@users.noreply.github.com>
2014-06-19 00:48:19 -0300

    compiLatex 3.0

--- compilatex.sh
+++ compilatex.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Script que compila automáticamente un archivo de LaTex cada vez que se guarda un cambio.
+# Script que compila automáticamente un archivo de LaTex cada vez que se guarda.
 # Para usarlo, ejecutar en la Terminal "sh compilatex.sh"
 
 # Para terminar de ejecutar el script, tipear en la Terminal "Ctrl + Shift + c".
-# Si el archivo no compila, arreglar el error y escribir "quit" (o "x") en la Terminal. 
-# El compilador va a tratar de compilarlo de nuevo. Si compila, el pdf se va a seguir actualizando!
+# Si el archivo no compila, arreglar el error y escribir "quit" (o "q") en la Terminal. 
+# El compilador va a tratar de compilarlo de nuevo. Si compila, el pdf se va a seguir actualizando cada vez que se guarde!
 
 
 read -p "Ingrese el nombre del archivo .TeX a compilar: " archivo
 
 
 pdflatex  $archivo.tex
-hash=$(openssl md5 $archivo.tex)
+fecha=$(date -r $archivo.tex)
 
 
 while true 
     do
-       if [ "$hash" != "$(openssl md5 $archivo.tex)" ]; then    
+       if [ "$fecha" != "$(date -r $archivo.tex)" ]; then    
        		pdflatex  $archivo.tex
-       		hash=$(openssl md5 $archivo.tex)
+       		fecha=$(date -r $archivo.tex)
        fi 
        sleep 1
     done
